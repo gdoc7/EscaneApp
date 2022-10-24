14fcb3a7c896f11df697e26d078eec5857e23658
gabriel <gabrielojeda7@gmail.com>
2022-10-23 21:11:42 -0400

    Acceso a camara y funcionalidades

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, View, Image } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import Button from "./src/components/Button";
 
 export default function App() {
+  const [hasCameraPermission, sethasCameraPermission] = useState(null);
+  const [image, setimage] = useState(null);
+  const [type, settype] = useState(CameraType.back);
+  const [Flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setimage(data.uri);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      sethasCameraPermission(cameraStatus.status === "granted");
+    })();
+  }, []);
+
+  if (hasCameraPermission === false) {
+    return <Text>No tiene permisos </Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!image ? (
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={Flash}
+          ref={cameraRef}
+        >
+          <View style={styles.topButtonsContainer}>
+            <Button icon={'flash'} color={Flash === Camera.Constants.FlashMode.off ? '#f1f1f1': 'yellow'} 
+              onPress={() => {
+              setFlash(Flash === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.on :  Camera.Constants.FlashMode.off)
+           
+              console.log("flash ", Flash);
+
+            }}/>
+
+          </View>
+        </Camera>
+      ) : (
+        <Image source={{ uri: image }} style={styles.camera} />
+      )}
+      <View>
+        {image ? (
+          <View style={styles.buttonsContainer}>
+            <Button
+              title={"Tomar otra"}
+              icon="retweet"
+              onPress={() => setimage(null)}
+            />
+            <Button title={"Guardar"} icon="check" />
+          </View>
+        ) : (
+          <Button
+            title={"Tomar foto"}
+            icon="camera"
+            onPress={takePicture}
+          ></Button>
+        )}
+      </View>
     </View>
   );
 }
@@ -13,8 +82,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    paddingBottom: 20,
+    justifyContent: "center",
   },
+  camera: {
+    flex: 1,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    // justifyContent:'space-between',
+    justifyContent: "space-between",
+    paddingHorizontal: 50,
+  },
+  topButtonsContainer:{
+    flexDirection:'row',
+    justifyContent:'end',
+    padding:45
+  }
 });
